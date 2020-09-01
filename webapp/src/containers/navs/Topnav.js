2acf6358550f2da16da6545e34f53e891f432fec
Juan Pablo Salazar <pablo.salazar.restrepo@gmail.com>
2020-08-31 21:13:11 -0500

    remove unused functions on topnav component

--- webapp/src/containers/navs/Topnav.js
+++ webapp/src/containers/navs/Topnav.js
@@ -16,10 +16,7 @@ import {
   logoutUser,
 } from '../../redux/actions';
 
-import {
-  menuHiddenBreakpoint,
-  searchPath,
-} from '../../constants/defaultValues';
+import { menuHiddenBreakpoint } from '../../constants/defaultValues';
 
 import { MobileMenuIcon, MenuIcon } from '../../components/svg';
 import TopnavEasyAccess from './Topnav.EasyAccess';
@@ -44,84 +41,6 @@ class TopNav extends Component {
         document.mozFullScreenElement !== null) ||
       (document.msFullscreenElement && document.msFullscreenElement !== null)
     );
-  };
-  handleSearchIconClick = (e) => {
-    if (window.innerWidth < menuHiddenBreakpoint) {
-      let elem = e.target;
-      if (!e.target.classList.contains('search')) {
-        if (e.target.parentElement.classList.contains('search')) {
-          elem = e.target.parentElement;
-        } else if (
-          e.target.parentElement.parentElement.classList.contains('search')
-        ) {
-          elem = e.target.parentElement.parentElement;
-        }
-      }
-
-      if (elem.classList.contains('mobile-view')) {
-        this.search();
-        elem.classList.remove('mobile-view');
-        this.removeEventsSearch();
-      } else {
-        elem.classList.add('mobile-view');
-        this.addEventsSearch();
-      }
-    } else {
-      this.search();
-    }
-  };
-  addEventsSearch = () => {
-    document.addEventListener('click', this.handleDocumentClickSearch, true);
-  };
-  removeEventsSearch = () => {
-    document.removeEventListener('click', this.handleDocumentClickSearch, true);
-  };
-
-  handleDocumentClickSearch = (e) => {
-    let isSearchClick = false;
-    if (
-      e.target &&
-      e.target.classList &&
-      (e.target.classList.contains('navbar') ||
-        e.target.classList.contains('simple-icon-magnifier'))
-    ) {
-      isSearchClick = true;
-      if (e.target.classList.contains('simple-icon-magnifier')) {
-        this.search();
-      }
-    } else if (
-      e.target.parentElement &&
-      e.target.parentElement.classList &&
-      e.target.parentElement.classList.contains('search')
-    ) {
-      isSearchClick = true;
-    }
-
-    if (!isSearchClick) {
-      const input = document.querySelector('.mobile-view');
-      if (input && input.classList) input.classList.remove('mobile-view');
-      this.removeEventsSearch();
-      this.setState({
-        searchKeyword: '',
-      });
-    }
-  };
-  handleSearchInputChange = (e) => {
-    this.setState({
-      searchKeyword: e.target.value,
-    });
-  };
-  handleSearchInputKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      this.search();
-    }
-  };
-
-  search = () => {
-    this.props.history.push(searchPath + '/' + this.state.searchKeyword);
-    this.setState({
-      searchKeyword: '',
-    });
   };
 
   toggleFullScreen = () => {
